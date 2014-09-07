62402f54aa7deabf740e284c927b2ba131212ad9
redaphid <aaron@loqwai.com>
2014-09-07 12:26:57 -0700

    stuff

--- awareness/heart.js
+++ awareness/heart.js
@@ -7,5 +7,31 @@ angular.module('sqrt', ["kendo.directives"])
         }, 100);
     })
     .controller('HeartController', function( $scope){
+        $scope.heartScale ={
+            minorUnit: 5,
+            startAngle: -30,
+            endAngle: 210,
+            min: 50,
+            max: 180,
+            labels: {
+                position: "inside"
+            },
+            ranges: [
+                {
+                    from: 50,
+                    to: 100,
+                    color: "#00ff00"
+                }
+                , {
+                    from: 100,
+                    to: 135,
+                    color: "#ff7a00"
+                }, {
+                    from: 135,
+                    to: 180,
+                    color: "#ff0000"
+                }
+            ]
+        };
 
     });
