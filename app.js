3ee62dd1b831d6fa95a7d526dee6204793caf3a3
redaphid <aaron@loqwai.com>
2014-09-06 17:31:11 -0700

    Added color generator based on bac (assuming 0-1)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var skynet = require('skynet');
 var uuids = {
-  nodeblu: '25944a21-3601-11e4-8e5a-919063640dc3',
+  alcohol: '25944a21-3601-11e4-8e5a-919063640dc3',
   mike: 'ca03a096-b9e3-4c7c-8e77-b85cf7ad1d2f',
+  koshin: 'effe7a0e-cb0f-4175-bc0e-3912749380d6',
   heart: 'd870d511-1c42-11e4-861d-89322229e557'
 };
 
@@ -35,14 +36,13 @@ conn.on('ready', function(data){
 
 
 function setHeartRate(heartRate) {
+    console.log('saw heart');
     if(!heartRate) {
         switchBlink(false);
         return;
     }
 
     var milliseconds = ((60/heartRate) * 1000);
-
-    console.log('milliseconds', milliseconds);
 
     if(heartInterval) {
         clearInterval(heartInterval);
@@ -51,6 +51,30 @@ function setHeartRate(heartRate) {
     heartInterval = setInterval(blinkBlink, milliseconds);
 }
 
+
+//expect bac to be between 0-1
+function bac2Color(bac) {
+
+    bac = bac * 255;
+    var base = 0x00FFFF, color = base;
+
+    //remove the 'blue' value the higher the bac is.
+    color -= bac;
+    //add the 'red' value the higher the bac is.
+    color += (bac << 16);
+
+    color = Math.round(color);
+
+    color = color.toString(16);
+
+    //zero-fill string
+    while(color.length < 6) {
+        color = '0' + color;
+    }
+    color = '#' + color;
+
+    return color;
+}
 
 function blinkBlink() {
     switchBlink(true);
@@ -65,7 +89,7 @@ function switchBlink(on) {
         subdevice: 'citi-blink',
         payload: {
             on: on,
-            rgb: '#CCCCCC'
+            rgb: '#CC0000'
         }
     });
 }
